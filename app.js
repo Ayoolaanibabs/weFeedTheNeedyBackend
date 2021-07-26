5300737635788834051ef55463cec5209dc46054
ayo <hakeemanibaba@yahoo.com>
2021-07-26 22:08:48 +0100

    changes to port config

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use((error, req, res, next)=>{
 */
 const PORT  = process.env.PORT || 7000;
 
-app.listen(7000, () => {
+app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 });
